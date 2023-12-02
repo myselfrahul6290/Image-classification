347ad9c89d5c1de92b53e463c547962e9cfca87a
Rahul Shaw <myselfrahul6290@gmail.com>
2023-12-02 14:03:05 +0530

    first commit

---
+++ TestCode/Extract_FolderName.py
@@ -0,0 +1,13 @@
+import os
+import pickle
+
+actors = os.listdir('data')
+
+filenames = []
+
+for actor in actors:
+    for file in os.listdir(os.path.join('data',actor)):
+        filenames.append(os.path.join('data',actor,file))
+
+# print(filenames)
+pickle.dump(filenames,open('filenames.pkl','wb'))
